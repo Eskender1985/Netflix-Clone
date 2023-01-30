1786323447b69092f3411ca648788b183f57d4ef
eskender_rakhimov <eskender.rakhimov@jetruby.com>
2023-01-30 19:34:52 +0700

    Created MainTabBarViewController

---
+++ Netflix Clone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadsViewController.swift
+//  Netflix Clone
+//
+//  Created by User on 30/01/2023.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by User on 30/01/2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
